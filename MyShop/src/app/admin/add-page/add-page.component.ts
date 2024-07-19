8cbe6e5b5d298a5dedfe2a3bad8a06069aa78d5c
LenaBulina <aries_07@ukr.net>
2024-07-19 12:14:56 +0300

    Created add-product form

--- MyShop/src/app/admin/add-page/add-page.component.ts
+++ MyShop/src/app/admin/add-page/add-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-page',
   templateUrl: './add-page.component.html',
   styleUrls: ['./add-page.component.scss']
 })
-export class AddPageComponent {
+export class AddPageComponent implements OnInit{
+  form: FormGroup
+  submitted: boolean = false
+
+  constructor() {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      type: new FormControl(null, Validators.required),
+      title: new FormControl(null, Validators.required),
+      photo: new FormControl(null, Validators.required),
+      info: new FormControl(null, Validators.required),
+      price: new FormControl(null, Validators.required),
+    })
+  }
+  
+  submit() {}
 
 }
+
+
