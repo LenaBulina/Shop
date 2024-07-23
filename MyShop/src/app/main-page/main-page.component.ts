36268b05be70e01d71c94307746af5b5e612f19e
LenaBulina <aries_07@ukr.net>
2024-07-23 17:55:00 +0300

    implemented getAll method. Created product component

--- MyShop/src/app/main-page/main-page.component.ts
+++ MyShop/src/app/main-page/main-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit{
+
+  products$
+
+  constructor( private productServ: ProductService) {}
+
+  ngOnInit() {
+    this.products$ = this.productServ.getAll();
+  }
 
 }
